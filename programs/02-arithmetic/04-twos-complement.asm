da0dc68bcea26127f26a002dbc1cd93b35e1584f
Anup Pyakurel <46782031+techgeek68@users.noreply.github.com>
2026-07-13 09:51:36 +0545

    Fix memory addresses for two's complement program

--- programs/02-arithmetic/04-twos-complement.asm
+++ programs/02-arithmetic/04-twos-complement.asm
@@ -1,6 +1,6 @@
 ; Program 8 Two's Complement of a Number
-        LDA  4200H      ; A gets the number
+        LDA  2000H      ; A gets the number
         CMA             ; A gets the one's complement
         ADI  01H        ; add one to get the two's complement
-        STA  4300H      ; store the result
+        STA  2003H      ; store the result
         HLT
