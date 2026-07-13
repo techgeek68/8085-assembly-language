e6d999ea3c3795b4d856d738cf2a28eb6a8cb24b
Anup Pyakurel <46782031+techgeek68@users.noreply.github.com>
2026-07-13 09:06:46 +0545

    Change storage address from 4000H to 9000H

--- programs/01-data-transfer/01-store-8bit-direct.asm
+++ programs/01-data-transfer/01-store-8bit-direct.asm
@@ -1,5 +1,5 @@
-; Program 1: Store 8 bit Data in Memory
+; Program 1 Store 8 bit Data in Memory
 ; Method 1, direct addressing
         MVI  A, 40H     ; A gets 40H
-        STA  4000H      ; store A at address 4000H
+        STA  9000      ; store A at address 9000H
         HLT             ; stop
