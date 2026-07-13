b1e7d0c0e071c92170deae155c2faea179430f55
Anup Pyakurel <46782031+techgeek68@users.noreply.github.com>
2026-07-13 09:47:42 +0545

    Update 05-add-block-with-carry.asm

--- programs/02-arithmetic/05-add-block-with-carry.asm
+++ programs/02-arithmetic/05-add-block-with-carry.asm
@@ -1,5 +1,5 @@
 ; Program 9 Addition of a Block of 8 bit Numbers with Carry
-        LXI  H, 6000H   ; base of the ten byte array
+        LXI  H, 2000H   ; base of the ten byte array
         MVI  B, 00H     ; carry counter, becomes the high byte
         MVI  C, 09H     ; nine more additions follow the first byte
         MOV  A, M       ; A gets the first element
@@ -9,7 +9,7 @@ UP:     INX  H          ; move to the next element
         INR  B          ; otherwise count the overflow
 SKIP:   DCR  C
         JNZ  UP
-        STA  9500H      ; low byte of the sum
+        STA  4000H      ; low byte of the sum
         MOV  A, B
-        STA  9501H      ; high byte, the carry count
+        STA  4005H      ; high byte, the carry count
         HLT
