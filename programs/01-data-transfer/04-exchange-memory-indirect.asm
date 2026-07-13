a4cbe52be5be4ca5e8c086f11bea1158de834a7f
Anup Pyakurel <46782031+techgeek68@users.noreply.github.com>
2026-07-13 09:13:05 +0545

    Update 04-exchange-memory-indirect.asm

--- programs/01-data-transfer/04-exchange-memory-indirect.asm
+++ programs/01-data-transfer/04-exchange-memory-indirect.asm
@@ -1,4 +1,4 @@
-; Program 4: Exchange Contents of Two Memory Locations (indirect addressing)
+; Program 4 Exchange Contents of Two Memory Locations (indirect addressing)
         LXI  H, 2000H   ; HL points to first location
         LXI  D, 4000H   ; DE points to second location
         MOV  B, M       ; B gets (2000H)
