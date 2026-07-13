bc0a101d575322b67b20a1d58f8d526c6e2d0cbb
Anup Pyakurel <46782031+techgeek68@users.noreply.github.com>
2026-07-13 09:43:04 +0545

    Update 04-twos-complement.asm

--- programs/02-arithmetic/04-twos-complement.asm
+++ programs/02-arithmetic/04-twos-complement.asm
@@ -1,4 +1,4 @@
-; Program 8: Two's Complement of a Number
+; Program 8 Two's Complement of a Number
         LDA  4200H      ; A gets the number
         CMA             ; A gets the one's complement
         ADI  01H        ; add one to get the two's complement
