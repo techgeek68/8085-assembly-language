3c5bd5b43dfb51ae5ccc07d3e9cdbb020a7ef739
Anup Pyakurel <46782031+techgeek68@users.noreply.github.com>
2026-07-13 09:42:50 +0545

    Update 03-ones-complement.asm

--- programs/02-arithmetic/03-ones-complement.asm
+++ programs/02-arithmetic/03-ones-complement.asm
@@ -1,4 +1,4 @@
-; Program 7: One's Complement of a Number
+; Program 7 One's Complement of a Number
         LDA  4400H      ; A gets the number
         CMA             ; A gets the one's complement of A
         STA  4300H      ; store the result
